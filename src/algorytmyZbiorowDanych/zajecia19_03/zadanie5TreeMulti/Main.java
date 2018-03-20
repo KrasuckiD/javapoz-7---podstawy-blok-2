215ff63bc36cf8479200f0bbc8616aec62e3781f
KrasuckiD <darasmm@gmail.com>
2018-03-20 17:26:28 +0100

    20.03

---
+++ src/algorytmyZbiorowDanych/zajecia19_03/zadanie5TreeMulti/Main.java
@@ -0,0 +1,21 @@
+package algorytmyZbiorowDanych.zajecia19_03.zadanie5TreeMulti;
+
+public class Main {
+    public static void main(String[] args) {
+        Node nodeA = new Node(null, "A");
+        Node nodeB = new Node(null, "B");
+        Node nodeC = new Node(null, "C");
+        Node nodeD = new Node(null, "D");
+        Node nodeE = new Node(null, "E");
+        Node nodeF = new Node(null, "F");
+        Node nodeG = new Node(null, "G");
+        Node nodeH = new Node(null, "H");
+
+        nodeA.addChild(nodeB);
+        nodeA.addChild(nodeC);
+        nodeA.addChild(nodeD);
+
+        nodeB.addChild(nodeE);
+
+    }
+}
---
+++ src/algorytmyZbiorowDanych/zajecia19_03/zadanie5TreeMulti/Tree.java
@@ -0,0 +1,7 @@
+package algorytmyZbiorowDanych.zajecia19_03.zadanie5TreeMulti;
+
+public class Tree {
+    private Node root;
+    private int count;
+
+}
